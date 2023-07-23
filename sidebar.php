0fe260dd627b0980b161c131cc36bfc79ab8f644
keijiek <kumatan800@gmail.com>
2023-07-23 14:36:41 +0900

    20230723

--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,72 @@
+<?php
+function display_menu_common(string $head_name):void {
 
+}
+
+function display_categories_menu(array $categories):void {
+?>
+  <section>
+    <h2 class="h4">カテゴリー</h2>
+    <ul>
+      <?php
+      foreach ($categories as $elm) {
+      ?>
+      <li><a href="<?= get_category_link($elm->term_id) ?>"><?= $elm->name ?>(<?= $elm->count?>)</a></li>
+      <?php
+      }
+      ?>
+    </ul>
+  </section>
+<?php
+}
+
+/**
+ * タグ一覧メニューの表示
+ */
+function display_tags_menu(array $tags):void {
+?>
+  <section>
+    <h2 class="h4">タグ</h2>
+    <ul>
+      <?php
+      foreach ($tags as $elm) {
+        ?>
+      <li><a href="<?= get_tag_link($elm->term_id) ?>"><?= $elm->name ?>(<?= $elm->count ?>)</a></li>
+        <?php
+      }
+      ?>
+    </ul>
+  </section>
+<?php
+}// タグ一覧メニュー表示の終了
+
+function display_month_list():void {
+
+}
+
+/**
+ * アサイド表示の本編
+ */
+?>
 <aside class="container-fluid">
-  <h2 class="h4">カテゴリー</h2>
-  <ul>
 <?php
-  $categories = get_categories();
-  foreach ($categories as $category) {
+// カテゴリー一覧の表示
+$categories = get_categories([
+  'taxonomy' => 'category',
+  'orderby ' => 'count',
+  'order' => 'ASC',
+  'hide_empty' => 'true',
+  'pad_counts' => 'true',
+  'hierarchical ' => 'true',
+]);
+
+if( count($categories) > 0 ) {
+  display_categories_menu($categories);
+}
+// タグ一覧の表示
+$tags = get_tags();
+if( count($tags) > 0 ) {
+  display_tags_menu($tags);
+}
 ?>
-    <li><a href="<?= get_category_link($category->term_id) ?>"><?= $category->name ?></a></li>
-<?php
-  }
-?>
-  </ul>
-  <h2 class="h4">タグ</h2>
-  <ul>
-<?php
-  $tags = get_tags();
-  foreach ($tags as $tag) {
-?>
-    <li><a href="<?= get_tag_link($tag->term_id) ?>"><?= $tag->name ?></a></li>
-<?php
-  }
-?>
-  </ul>
 </aside>
